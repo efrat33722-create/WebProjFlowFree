09b1e92d2ad91744afd1f71b7a68ac0243d40e0d
TehilaAlmer <tehilaalmer@gmail.com>
2026-02-18 16:15:42 +0200

    upload proj3

---
+++ FlowFreeProject/game.js
@@ -0,0 +1,42 @@
+const params = new URLSearchParams(window.location.search);
+const id = Number(params.get("level"));  // המרה למספר
+
+console.log("Level ID:", id);
+let gameLevels = [
+
+    // שלב 1
+    [
+        { color: "red",    start: {row:0,col:0}, end: {row:4,col:1} },
+        { color: "blue",   start: {row:1,col:2}, end: {row:4,col:2} },
+        { color: "green",  start: {row:3,col:1}, end: {row:0,col:2} },
+        { color: "yellow", start: {row:0,col:4}, end: {row:3,col:3} },
+        { color: "orange", start: {row:1,col:4}, end: {row:4,col:3} }
+    ]
+];
+
+let currentLevel = gameLevels[id-1];
+console.log(gameLevels);
+console.log(currentLevel);
+let gameBoard = document.getElementById("game-container");
+gameBoard.style.gridTemplateColumns = `repeat(${currentLevel.length}, 1fr)`;
+gameBoard.style.gridTemplateRows = `repeat(${currentLevel.length}, 1fr)`;
+const size = currentLevel.length; 
+let cells = [];
+for (let i = 0; i < size; i++) {
+    cells[i] = [];
+    for (let j = 0; j < size; j++) {
+        let cell = document.createElement("div");
+        cell.style.border = "1px solid #ccc";
+        cell.style.width = "50px";
+        cell.style.height = "50px";
+        gameBoard.appendChild(cell);
+        cells[i][j] = cell;
+    }
+}
+
+// צבע את נקודות ההתחלה והסיום של כל צבע
+for (let colorObj of currentLevel) {
+    let {start, end, color} = colorObj;
+    cells[start.row][start.col].style.backgroundColor = color;
+    cells[end.row][end.col].style.backgroundColor = color;
+}
--- FlowFreeProject/opening.js
+++ FlowFreeProject/opening.js
@@ -1,19 +1,27 @@
 function createLevels(containerId, colorClass) {
     const grid = document.getElementById(containerId);
+    let cnt=1;
 
-    for (let i = 1; i <= 30; i++) {
+    for (let i = 1; i <= 5; i++) {
         const level = document.createElement("div");
         level.classList.add("level", colorClass);
         level.textContent = i;
+        level.id=cnt.toString();
+        cnt++;
 
         level.addEventListener("click", () => {
-            level.classList.toggle("completed");
+            
             console.log("Start level", i);
+            goToPage(event.target.id);
         });
 
         grid.appendChild(level);
     }
 }
+function goToPage(id) {
+    window.location.href = "game.html?level=" + id;
+}
+
 
 createLevels("grid5", "red");
 createLevels("grid6", "green");
